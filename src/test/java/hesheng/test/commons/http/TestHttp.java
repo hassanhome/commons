c2ac1429112576aa10a892172126e353c036902b
hassanhome <1067912178@qq.com>
2017-06-11 13:32:23 +0800

    httpconfig

--- src/test/java/hesheng/test/commons/http/TestHttp.java
+++ src/test/java/hesheng/test/commons/http/TestHttp.java
@@ -1,12 +1,8 @@
 package hesheng.test.commons.http;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.codec.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponseInterceptor;
@@ -21,8 +17,6 @@ import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-import hesheng.commons.http.HttpPostUtil;
-import hesheng.commons.http.HttpResponse;
 import hesheng.commons.http.MyHttpConfig;
 
 public class TestHttp {
@@ -61,56 +55,8 @@ public class TestHttp {
 		}
 	}
 
-	@Test
-	public void test1() throws ClientProtocolException, URISyntaxException, IOException{
-		Map<String, String> params = new HashMap<>();
-		params.put("name", "小明");
-		params.put("password", "这是密码"); 
-		HttpResponse resp = HttpPostUtil.getResponseByPost("http://localhost:8080/spmv/user/sayhello.do", params, Charsets.UTF_8);
-		System.out.println(resp.getBody());
-	}
-	
-	@Test
-	public void test2() throws ClientProtocolException, IOException{  
-		HttpUriRequest request = RequestBuilder.post("http://localhost:8080/spmv/user/sayhello.do")
-		              .addParameter("name", "小明")
-		              .addParameter("password", "这是密码" )
-		              .setCharset(StandardCharsets.UTF_8)   
-		              .build();
-		
-		  
-		CloseableHttpResponse resp = HttpClients.createDefault().execute(request);
-		System.out.println(EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8));
-		
-		
-	}
-	
-	@Test
-	public void test3() throws ClientProtocolException, IOException{
-		HttpResponseInterceptor itcp = new HttpResponseInterceptor() {
-			
-			@Override
-			public void process(org.apache.http.HttpResponse response, HttpContext context) throws HttpException, IOException {
-				System.out.println("thread="+Thread.currentThread().getId()+",responed :" + EntityUtils.toString(response.getEntity()));
-				
-			}
-		};
-		;
-		
-		HttpUriRequest request = RequestBuilder.post("http://localhost:8080/spmv/user/sayhello.do")
-        .addParameter("name", "小明")
-        .addParameter("password", "这是密码" )
-        .setCharset(StandardCharsets.UTF_8)  
-        .build();
 
-
-		CloseableHttpResponse resp = HttpClientBuilder.create()
-				                                      .addInterceptorFirst(itcp )
-				                                      .build()
-				                                      .execute(request);
-		System.out.println("thread="+Thread.currentThread().getId()+",executed.....");
-		System.out.println(EntityUtils.toString(resp.getEntity(), StandardCharsets.UTF_8));
-	}
+	
 	
 	 
 }
